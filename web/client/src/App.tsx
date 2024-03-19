f805f1dfc11869206199559e8a03f2d7d06de2d6
chreamy <chrislinkou@gmail.com>
2024-03-18 20:01:58 -0500

    client starter code

---
+++ web/client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/interview" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ web/client/src/pages/home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Home() {
+  return <h1>aaa</h1>;
+}
+export default Home;
