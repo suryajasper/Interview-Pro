901aebae1f8372708bc1b61fa2bbb7dd7eadb8f6
John Mo <peikaimo@gmail.com>
2024-03-21 00:22:53 -0500

    added new analysis page

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Analysis from "./pages/analysis";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/interview" element={<Home />} />
+        <Route path="/analysis" element={<Analysis />} />
       </Routes>
     </Router>
   );
---
+++ web/client/src/pages/analysis.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Spline, { SplineEvent } from "@splinetool/react-spline";
+import "../App.css";
+
+
+class Analysis extends React.Component {
+    render() {
+        return (
+            <div className="container">
+
+
+            </div>
+        );
+    }
+
+
+}
+
+export default Analysis;
