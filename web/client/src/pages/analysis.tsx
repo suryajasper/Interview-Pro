56998469f5bd4862dc89426167457f01139cb6a0
John Mo <peikaimo@gmail.com>
2024-03-21 00:47:15 -0500

    boxes for analysis

--- web/client/src/pages/analysis.tsx
+++ web/client/src/pages/analysis.tsx
@@ -7,7 +7,23 @@ class Analysis extends React.Component {
     render() {
         return (
             <div className="container">
+                <div className="header">
+                    <h1>Stats Review</h1>
+                </div>
 
+                <div className="graph">
+                    <p>starfish</p>
+                </div>
+
+                <div >
+                    <div className="gptFeedback">
+                        <p>feedback</p>
+                    </div>
+
+                    <div className="transcript">
+                        <p>transcript</p>
+                    </div>
+                </div>
 
             </div>
         );
